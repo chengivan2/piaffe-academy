0c73cc45253d2394df836c77016bf02189c892e7
Ivan Mulindo <chengoleivan@gmail.com>
2025-09-02 18:46:50 +0300

    Created inner text component

---
+++ src/app/components/sections/HeroContent.ts
@@ -0,0 +1,10 @@
+// Text content for the Hero section
+const heroContent = {
+  heading: 'HORSES HAVEN',
+  subheading: 'Excellence in equine care',
+  description: `Discover our world-class facilities, dedicated to the health, happiness, and success of every horse.\nExplore our services and join a legacy.`,
+  bookNow: 'Book Now',
+  scrollText: 'Scroll down to discover more',
+};
+
+export default heroContent;
