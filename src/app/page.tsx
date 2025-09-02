2c1f4761eed67c560c1ba19c9811962abc9ae16a
Ivan Mulindo <chengoleivan@gmail.com>
2025-09-02 22:03:11 +0300

    Added the cta section to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import CTASection from "./components/sections/CTASection";
 import Hero from "./components/sections/Hero";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <CTASection />
     </main>
   );
 }
