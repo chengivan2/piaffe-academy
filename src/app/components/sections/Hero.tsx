206104e6918ef9b7c454632d72149450f9a0cb0e
Ivan Mulindo <chengoleivan@gmail.com>
2025-09-02 12:02:45 +0300

    Added Hero section skeleton, required images, and loading gif

---
+++ src/app/components/sections/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <section className="relative min-w-full min-h-[100vh] bg-[url('/equestrian-horse-jump.jpg')] bg-cover bg-center">
+
+    </section>
+  )
+}
