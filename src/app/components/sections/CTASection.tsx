7a8c5bde7c021de1ba082e05b7af3c7fb4970b54
Ivan Mulindo <chengoleivan@gmail.com>
2025-09-03 12:28:40 +0300

    Modified the CTASection percentage numbers

--- src/app/components/sections/CTASection.tsx
+++ src/app/components/sections/CTASection.tsx
@@ -7,11 +7,11 @@ export default function CTASection() {
     <section className="relative min-w-full min-h-[100vh] flex items-center justify-center bg-[url('/two-horses.jpg')] bg-cover bg-center bg-fixed">
       <div className="absolute inset-0 bg-black/80 bg-opacity-20 z-10" />
       <div className="relative z-20 flex flex-col items-center justify-center text-center px-6 py-16">
-        <div className="mb-6 flex flex-col items-center">
+        <div className="absolute top-1 left-1 mb-6 flex flex-col items-center">
           <span className="cta-small-percent text-gray-400 text-2xl md:text-3xl font-bold drop-shadow-md tracking-tight line-through">
             10%
           </span>
-          <span className="cta-big-percent text-white text-5xl md:text-7xl font-black drop-shadow-lg tracking-tight">
+          <span className="cta-big-percent text-blue-600 text-5xl md:text-7xl font-black drop-shadow-lg tracking-tight">
             20%
           </span>
         </div>
