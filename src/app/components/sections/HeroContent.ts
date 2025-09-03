cd7c7511b50a8276955b288a6165f0fb4d5393d6
Ivan Mulindo <chengoleivan@gmail.com>
2025-09-03 11:30:30 +0300

    Modified hero CTA message

--- src/app/components/sections/HeroContent.ts
+++ src/app/components/sections/HeroContent.ts
@@ -2,7 +2,7 @@ const heroContent = {
   heading: 'PIAFFE ACADEMY',
   subheading: 'Excellence in equine care',
   description: `Discover our world-class facilities, dedicated to the health, happiness, and success of every horse.\nExplore our services and join a legacy.`,
-  bookNow: 'Book Now',
+  bookNow: 'Start Classes',
 };
 
 export default heroContent;
