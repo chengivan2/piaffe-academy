94266fd2432e19ce57539f782e1c0fcf73553d21
Ivan Mulindo <chengoleivan@gmail.com>
2025-09-03 08:51:28 +0300

    Displayed the footer in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import CTASection from "./components/sections/CTASection";
 import Hero from "./components/sections/Hero";
 import DelayedLoading from "./components/DelayedLoading";
+import Footer from "./components/sections/Footer";
 
 export default function Home() {
   return (
-    <DelayedLoading minDuration={3000}>
+    <DelayedLoading minDuration={3600}>
       <main>
         <Hero />
         <CTASection />
       </main>
+      <Footer />
     </DelayedLoading>
   );
 }
